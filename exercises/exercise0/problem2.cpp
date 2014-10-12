50caa1f5918acf809a604b7fd547fe76d5061c67
elementary <elementary@nikozavar@mail.bg>
2014-10-12 13:57:24 +0300

    Added solutions to the first two problems

---
+++ exercises/exercise0/problem2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber;
+
+    cout << "First number = ";
+    cin >> firstNumber;
+
+    cout << "second number = "
+    cin >> secondNumber;
+
+    cout << firstNumber + secondNumber << endl;
+    return 0;
+}
